dc1f51f35149b953c7f00034b37c4cd5e58fc2e1
«Jeaninne» <bokhan.e@gmail.com>
2012-12-06 08:54:30 +0200

    modified:   config.ru 	modified:   graber.rb

--- config.ru
+++ config.ru
@@ -1,4 +1,15 @@
 require './graber.rb'
-use Rack::Reloader
 
-run Link_graber.new
+app = Rack::Builder.new do
+ use Rack::Reloader
+
+  run Link_graber.new
+
+  map '/e' do
+   run Proc.new {|env| [200, {"Content-Type" => "text/html"}, ["kjbhklb"]] }
+  end
+end
+
+
+Rack::Handler::WEBrick.run app, :Port => 9292
+
--- graber.rb
+++ graber.rb
@@ -17,19 +17,48 @@ class Link_graber
 <body style="background: #EEEEEE;"> 
  <form>
     <p>
-    Enter site: <input type="text" name="page_neme"><br>
-    <input type="submit" value="parse it!">
+    Enter site: <input type="text" name="page_name"><br>
+    Enter depth: <input type="text" name="depth"><br>
+    <input type="submit" value="parse it!" action="action.html" method="get">
     </p>
  </form>
 </body>
 </html>
-
 html
 
     status, headers = 200, {"Content-Type" => "text/html"}
     [status, headers, [form]]
   end
 
+
+
+
+#  def #blyad
+#    #page_name
+#  end
+
+ def parse(page_name)
+  links = []
+  doc = Nokogiri::HTML(open(page_name))
+   doc.xpath('//a').each do |link|
+   if !((link[:href]=='#') || (link[:href]== nil)) then
+     if link[:href].include?('http')
+      then links << link[:href]
+           end
+     end
+  end
+  links
+ end
+
+ def find_links(page_name, depth)
+  File.open('data.txt', 'a'){ |file| file.puts  "#{depth} depht. Links on #{page_name}:" }
+  links_array = parse(page_name)
+  if depth > 1 then
+   links_array.each {|page| find_links(page, depth-1)
+    }#write on page
+  else links_array.each {|page| } #write on page
+   end
+ end
 
 end
 
